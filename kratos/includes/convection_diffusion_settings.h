9f739b0f74165adfb860a098befd4283551a0b65
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2021-04-12 18:31:31 +0200

    Testing (not working)

--- kratos/includes/convection_diffusion_settings.h
+++ kratos/includes/convection_diffusion_settings.h
@@ -375,30 +375,7 @@ protected:
     ///@}
     ///@name Protected member Variables
     ///@{
-    const Variable<double>* mpDensityVar;
-    const Variable<double>* mpDiffusionVar;
-    const Variable<double>* mpUnknownVar;
-    const Variable<double>* mpVolumeSourceVar;
-    const Variable<double>* mpSurfaceSourceVar;
-    const Variable<double>* mpProjectionVar;
-    const Variable<array_1d<double,3> >* mpConvectionVar;
-    const Variable<array_1d<double,3> >* mpMeshVelocityVar;
-    const Variable<double>* mpTransferCoefficientVar;
-    const Variable<array_1d<double,3> >* mpVelocityVar;
-    const Variable<double>* mpSpecificHeatVar;
-    const Variable<double>* mpReactionVar;
-    bool mis_defined_DensityVar;
-    bool mis_defined_DiffusionVar;
-    bool mis_defined_UnknownVar;
-    bool mis_defined_VolumeSourceVar;
-    bool mis_defined_SurfaceSourceVar;
-    bool mis_defined_ProjectionVar;
-    bool mis_defined_ConvectionVar;
-    bool mis_defined_MeshVelocityVar;
-    bool mis_defined_TransferCoefficientVar;
-    bool mis_defined_VelocityVar;
-    bool mis_defined_SpecificHeatVar;
-    bool mis_defined_ReactionVar;
+
 
     ///@}
     ///@name Protected Operators
@@ -436,6 +413,30 @@ private:
     ///@name Member Variables
     ///@{
 
+    const Variable<double>* mpDensityVar = nullptr;
+    const Variable<double>* mpDiffusionVar = nullptr;
+    const Variable<double>* mpUnknownVar = nullptr;
+    const Variable<double>* mpVolumeSourceVar = nullptr;
+    const Variable<double>* mpSurfaceSourceVar = nullptr;
+    const Variable<double>* mpProjectionVar = nullptr;
+    const Variable<array_1d<double,3> >* mpConvectionVar = nullptr;
+    const Variable<array_1d<double,3> >* mpMeshVelocityVar = nullptr;
+    const Variable<double>* mpTransferCoefficientVar = nullptr;
+    const Variable<array_1d<double,3> >* mpVelocityVar = nullptr;
+    const Variable<double>* mpSpecificHeatVar = nullptr;
+    const Variable<double>* mpReactionVar = nullptr;
+    bool mis_defined_DensityVar = false;
+    bool mis_defined_DiffusionVar = false;
+    bool mis_defined_UnknownVar = false;
+    bool mis_defined_VolumeSourceVar = false;
+    bool mis_defined_SurfaceSourceVar = false;
+    bool mis_defined_ProjectionVar = false;
+    bool mis_defined_ConvectionVar = false;
+    bool mis_defined_MeshVelocityVar = false;
+    bool mis_defined_TransferCoefficientVar = false;
+    bool mis_defined_VelocityVar = false;
+    bool mis_defined_SpecificHeatVar = false;
+    bool mis_defined_ReactionVar = false;
 
     ///@}
     ///@name Private Operators
@@ -450,79 +451,103 @@ private:
 
     virtual void save(Serializer& rSerializer) const
     {
-        rSerializer.save("DensityVarSaved", mpDensityVar != nullptr && mpDensityVar->Name() != "");
-        if (mpDensityVar != nullptr && mpDensityVar->Name() != "")
-        {
+        if (mpDensityVar != nullptr) {
             rSerializer.save("DensityVar",mpDensityVar);
         }
-        rSerializer.save("DiffusionVarSaved", mpDiffusionVar != nullptr && mpDiffusionVar->Name() != "");
-        if (mpDiffusionVar != nullptr && mpDiffusionVar->Name() != "")
-        {
+        if (mpDiffusionVar != nullptr) {
             rSerializer.save("DiffusionVar",mpDiffusionVar);
         }
-        rSerializer.save("UnknownVarSaved", mpUnknownVar != nullptr && mpUnknownVar->Name() != "");
-        if (mpUnknownVar != nullptr && mpUnknownVar->Name() != "")
-        {
+        if (mpUnknownVar != nullptr) {
             rSerializer.save("UnknownVar",mpUnknownVar);
         }
-        rSerializer.save("VolumeSourceVarSaved", mpVolumeSourceVar != nullptr && mpVolumeSourceVar->Name() != "");
-        if (mpVolumeSourceVar != nullptr && mpVolumeSourceVar->Name() != "")
-        {
+        if (mpVolumeSourceVar != nullptr) {
             rSerializer.save("VolumeSourceVar",mpVolumeSourceVar);
         }
-        rSerializer.save("SurfaceSourceVarSaved", mpSurfaceSourceVar != nullptr && mpSurfaceSourceVar->Name() != "");
-        if ( mpSurfaceSourceVar != nullptr && mpSurfaceSourceVar->Name() != "")
-        {
+        if ( mpSurfaceSourceVar != nullptr) {
             rSerializer.save("SurfaceSourceVar",mpSurfaceSourceVar);
         }
-        rSerializer.save("ProjectionVarSaved", mpProjectionVar != nullptr && mpProjectionVar->Name() != "");
-        if ( mpProjectionVar != nullptr && mpProjectionVar->Name() != "")
-        {
+        if ( mpProjectionVar != nullptr) {
             rSerializer.save("ProjectionVar",mpProjectionVar);
         }
-        rSerializer.save("ConvectionVarSaved", mpConvectionVar != nullptr && mpConvectionVar->Name() != "");
-        if ( mpConvectionVar != nullptr && mpConvectionVar->Name() != "")
-        {
+        if ( mpConvectionVar != nullptr) {
             rSerializer.save("ConvectionVar",mpConvectionVar);
         }
-        rSerializer.save("MeshVelocityVarSaved", mpMeshVelocityVar != nullptr && mpMeshVelocityVar->Name() != "");
-        if (mpMeshVelocityVar != nullptr && mpMeshVelocityVar->Name() != "")
-        {
+        if (mpMeshVelocityVar != nullptr) {
             rSerializer.save("MeshVelocityVar",mpMeshVelocityVar);
         }
-        rSerializer.save("TransferCoefficientVarSaved", mpTransferCoefficientVar != nullptr && mpTransferCoefficientVar->Name() != "");
-        if ( mpTransferCoefficientVar != nullptr && mpTransferCoefficientVar->Name() != "")
-        {
+        if ( mpTransferCoefficientVar != nullptr) {
             rSerializer.save("TransferCoefficientVar",mpTransferCoefficientVar);
         }
-        rSerializer.save("VelocityVarSaved", mpVelocityVar != nullptr && mpVelocityVar->Name() != "");
-        if (mpVelocityVar != nullptr && mpVelocityVar->Name() != "")
-        {
+        if (mpVelocityVar != nullptr) {
             rSerializer.save("VelocityVar",mpVelocityVar);
         }
-        rSerializer.save("SpecificHeatVarSaved", mpSpecificHeatVar != nullptr && mpSpecificHeatVar->Name() != "");
-        if (mpSpecificHeatVar != nullptr && mpSpecificHeatVar->Name() != "")
-        {
+        if (mpSpecificHeatVar != nullptr) {
             rSerializer.save("SpecificHeatVar",mpSpecificHeatVar);
         }
-        rSerializer.save("ReactionVarSaved", mpReactionVar != nullptr && mpReactionVar->Name() != "");
-        if (mpReactionVar != nullptr && mpReactionVar->Name() != "")
-        {
+        if (mpReactionVar != nullptr) {
             rSerializer.save("ReactionVar",mpReactionVar);
         }
 
-        // rSerializer.save("DensityVar",mpDensityVar);
-        // rSerializer.save("DiffusionVar",mpDiffusionVar);
-        // rSerializer.save("UnknownVar",mpUnknownVar);
-        // rSerializer.save("VolumeSourceVar",mpVolumeSourceVar);
-        // rSerializer.save("SurfaceSourceVar",mpSurfaceSourceVar);
-        // rSerializer.save("ProjectionVar",mpProjectionVar);
-        // rSerializer.save("ConvectionVar",mpConvectionVar);
-        // rSerializer.save("MeshVelocityVar",mpMeshVelocityVar);
-        // rSerializer.save("TransferCoefficientVar",mpTransferCoefficientVar);
-		// rSerializer.save("VelocityVar",mpVelocityVar);
- 		// rSerializer.save("SpecificHeatVar",mpSpecificHeatVar);
-        // rSerializer.save("ReactionVar",mpReactionVar);
+        // rSerializer.save("DensityVarSaved", mpDensityVar != nullptr && mpDensityVar->Name() != "");
+        // if (mpDensityVar != nullptr && mpDensityVar->Name() != "")
+        // {
+        //     rSerializer.save("DensityVar",mpDensityVar);
+        // }
+        // rSerializer.save("DiffusionVarSaved", mpDiffusionVar != nullptr && mpDiffusionVar->Name() != "");
+        // if (mpDiffusionVar != nullptr && mpDiffusionVar->Name() != "")
+        // {
+        //     rSerializer.save("DiffusionVar",mpDiffusionVar);
+        // }
+        // rSerializer.save("UnknownVarSaved", mpUnknownVar != nullptr && mpUnknownVar->Name() != "");
+        // if (mpUnknownVar != nullptr && mpUnknownVar->Name() != "")
+        // {
+        //     rSerializer.save("UnknownVar",mpUnknownVar);
+        // }
+        // rSerializer.save("VolumeSourceVarSaved", mpVolumeSourceVar != nullptr && mpVolumeSourceVar->Name() != "");
+        // if (mpVolumeSourceVar != nullptr && mpVolumeSourceVar->Name() != "")
+        // {
+        //     rSerializer.save("VolumeSourceVar",mpVolumeSourceVar);
+        // }
+        // rSerializer.save("SurfaceSourceVarSaved", mpSurfaceSourceVar != nullptr && mpSurfaceSourceVar->Name() != "");
+        // if ( mpSurfaceSourceVar != nullptr && mpSurfaceSourceVar->Name() != "")
+        // {
+        //     rSerializer.save("SurfaceSourceVar",mpSurfaceSourceVar);
+        // }
+        // rSerializer.save("ProjectionVarSaved", mpProjectionVar != nullptr && mpProjectionVar->Name() != "");
+        // if ( mpProjectionVar != nullptr && mpProjectionVar->Name() != "")
+        // {
+        //     rSerializer.save("ProjectionVar",mpProjectionVar);
+        // }
+        // rSerializer.save("ConvectionVarSaved", mpConvectionVar != nullptr && mpConvectionVar->Name() != "");
+        // if ( mpConvectionVar != nullptr && mpConvectionVar->Name() != "")
+        // {
+        //     rSerializer.save("ConvectionVar",mpConvectionVar);
+        // }
+        // rSerializer.save("MeshVelocityVarSaved", mpMeshVelocityVar != nullptr && mpMeshVelocityVar->Name() != "");
+        // if (mpMeshVelocityVar != nullptr && mpMeshVelocityVar->Name() != "")
+        // {
+        //     rSerializer.save("MeshVelocityVar",mpMeshVelocityVar);
+        // }
+        // rSerializer.save("TransferCoefficientVarSaved", mpTransferCoefficientVar != nullptr && mpTransferCoefficientVar->Name() != "");
+        // if ( mpTransferCoefficientVar != nullptr && mpTransferCoefficientVar->Name() != "")
+        // {
+        //     rSerializer.save("TransferCoefficientVar",mpTransferCoefficientVar);
+        // }
+        // rSerializer.save("VelocityVarSaved", mpVelocityVar != nullptr && mpVelocityVar->Name() != "");
+        // if (mpVelocityVar != nullptr && mpVelocityVar->Name() != "")
+        // {
+        //     rSerializer.save("VelocityVar",mpVelocityVar);
+        // }
+        // rSerializer.save("SpecificHeatVarSaved", mpSpecificHeatVar != nullptr && mpSpecificHeatVar->Name() != "");
+        // if (mpSpecificHeatVar != nullptr && mpSpecificHeatVar->Name() != "")
+        // {
+        //     rSerializer.save("SpecificHeatVar",mpSpecificHeatVar);
+        // }
+        // rSerializer.save("ReactionVarSaved", mpReactionVar != nullptr && mpReactionVar->Name() != "");
+        // if (mpReactionVar != nullptr && mpReactionVar->Name() != "")
+        // {
+        //     rSerializer.save("ReactionVar",mpReactionVar);
+        // }
 
         rSerializer.save("mis_defined_DensityVar",mis_defined_DensityVar);
         rSerializer.save("mis_defined_DiffusionVar",mis_defined_DiffusionVar);
@@ -542,92 +567,78 @@ private:
 
     virtual void load(Serializer& rSerializer)
     {
-        bool denstityvartoload;
-            rSerializer.load("DensityVarSaved", denstityvartoload);
-        if(denstityvartoload != false)
-        {
-            rSerializer.load("DensityVar",mpDensityVar);
-        }
-        bool diffusionvartoload;
-            rSerializer.load("DiffusionVarSaved", diffusionvartoload);
-        if(diffusionvartoload != false)
-        {
-            rSerializer.load("DiffusionVar",mpDiffusionVar);
-        }
-        bool unknownvartoload;
-            rSerializer.load("UnknownVarSaved", unknownvartoload);
-        if(unknownvartoload != false)
-        {
-            rSerializer.load("UnknownVar",mpUnknownVar);
-        }
-        bool volumesourcevartoload;
-            rSerializer.load("VolumeSourceVarSaved", volumesourcevartoload);
-        if(volumesourcevartoload != false)
-        {
-            rSerializer.load("VolumeSourceVar",mpVolumeSourceVar);
-        }
-        bool surfacesourcevartoload;
-            rSerializer.load("SurfaceSourceVarSaved", surfacesourcevartoload);
-        if(surfacesourcevartoload != false)
-        {
-            rSerializer.load("SurfaceSourceVar",mpSurfaceSourceVar);
-        }
-        bool projectionvartoload;
-            rSerializer.load("ProjectionVarSaved", projectionvartoload);
-        if(projectionvartoload != false)
-        {
-            rSerializer.load("ProjectionVar",mpProjectionVar);
-        }
-        bool convectionvartoload;
-            rSerializer.load("ConvectionVarSaved", convectionvartoload);
-        if(convectionvartoload != false)
-        {
-            rSerializer.load("ConvectionVar",mpConvectionVar);
-        }
-        bool meshvelocityvartoload;
-            rSerializer.load("MeshVelocityVarSaved", meshvelocityvartoload);
-        if(meshvelocityvartoload != false)
-        {
-            rSerializer.load("MeshVelocityVar",mpMeshVelocityVar);
-        }
-        bool transfercoefficientvartoload;
-            rSerializer.load("TransferCoefficientVarSaved", transfercoefficientvartoload);
-        if(transfercoefficientvartoload != false)
-        {
-            rSerializer.load("TransferCoefficientVar",mpTransferCoefficientVar);
-        }
-        bool velocityvartoload;
-            rSerializer.load("VelocityVarSaved", velocityvartoload);
-        if(velocityvartoload != false)
-        {
-            rSerializer.load("VelocityVar",mpVelocityVar);
-        }
-        bool specificheatvartoload;
-            rSerializer.load("SpecificHeatVarSaved", specificheatvartoload);
-        if(specificheatvartoload != false)
-        {
-            rSerializer.load("SpecificHeatVar",mpSpecificHeatVar);
-        }
-        bool reactionvartoload;
-            rSerializer.load("ReactionVarSaved", reactionvartoload);
-        if(reactionvartoload != false)
-        {
-            rSerializer.load("ReactionVar",mpReactionVar);
-        }
-
-
-        // rSerializer.load("DensityVar",mpDensityVar);
-        // rSerializer.load("DiffusionVar",mpDiffusionVar);
-        // rSerializer.load("UnknownVar",mpUnknownVar);
-        // rSerializer.load("VolumeSourceVar",mpVolumeSourceVar);
-        // rSerializer.load("SurfaceSourceVar",mpSurfaceSourceVar);
-        // rSerializer.load("ProjectionVar",mpProjectionVar);
-        // rSerializer.load("ConvectionVar",mpConvectionVar);
-        // rSerializer.load("MeshVelocityVar",mpMeshVelocityVar);
-        // rSerializer.load("TransferCoefficientVar",mpTransferCoefficientVar);
-        // rSerializer.load("VelocityVar",mpVelocityVar);
-		// rSerializer.load("SpecificHeatVar",mpSpecificHeatVar);
-        // rSerializer.load("ReactionVar",mpReactionVar);
+        // bool denstityvartoload;
+        //     rSerializer.load("DensityVarSaved", denstityvartoload);
+        // if(denstityvartoload != false)
+        // {
+        //     rSerializer.load("DensityVar",mpDensityVar);
+        // }
+        // bool diffusionvartoload;
+        //     rSerializer.load("DiffusionVarSaved", diffusionvartoload);
+        // if(diffusionvartoload != false)
+        // {
+        //     rSerializer.load("DiffusionVar",mpDiffusionVar);
+        // }
+        // bool unknownvartoload;
+        //     rSerializer.load("UnknownVarSaved", unknownvartoload);
+        // if(unknownvartoload != false)
+        // {
+        //     rSerializer.load("UnknownVar",mpUnknownVar);
+        // }
+        // bool volumesourcevartoload;
+        //     rSerializer.load("VolumeSourceVarSaved", volumesourcevartoload);
+        // if(volumesourcevartoload != false)
+        // {
+        //     rSerializer.load("VolumeSourceVar",mpVolumeSourceVar);
+        // }
+        // bool surfacesourcevartoload;
+        //     rSerializer.load("SurfaceSourceVarSaved", surfacesourcevartoload);
+        // if(surfacesourcevartoload != false)
+        // {
+        //     rSerializer.load("SurfaceSourceVar",mpSurfaceSourceVar);
+        // }
+        // bool projectionvartoload;
+        //     rSerializer.load("ProjectionVarSaved", projectionvartoload);
+        // if(projectionvartoload != false)
+        // {
+        //     rSerializer.load("ProjectionVar",mpProjectionVar);
+        // }
+        // bool convectionvartoload;
+        //     rSerializer.load("ConvectionVarSaved", convectionvartoload);
+        // if(convectionvartoload != false)
+        // {
+        //     rSerializer.load("ConvectionVar",mpConvectionVar);
+        // }
+        // bool meshvelocityvartoload;
+        //     rSerializer.load("MeshVelocityVarSaved", meshvelocityvartoload);
+        // if(meshvelocityvartoload != false)
+        // {
+        //     rSerializer.load("MeshVelocityVar",mpMeshVelocityVar);
+        // }
+        // bool transfercoefficientvartoload;
+        //     rSerializer.load("TransferCoefficientVarSaved", transfercoefficientvartoload);
+        // if(transfercoefficientvartoload != false)
+        // {
+        //     rSerializer.load("TransferCoefficientVar",mpTransferCoefficientVar);
+        // }
+        // bool velocityvartoload;
+        //     rSerializer.load("VelocityVarSaved", velocityvartoload);
+        // if(velocityvartoload != false)
+        // {
+        //     rSerializer.load("VelocityVar",mpVelocityVar);
+        // }
+        // bool specificheatvartoload;
+        //     rSerializer.load("SpecificHeatVarSaved", specificheatvartoload);
+        // if(specificheatvartoload != false)
+        // {
+        //     rSerializer.load("SpecificHeatVar",mpSpecificHeatVar);
+        // }
+        // bool reactionvartoload;
+        //     rSerializer.load("ReactionVarSaved", reactionvartoload);
+        // if(reactionvartoload != false)
+        // {
+        //     rSerializer.load("ReactionVar",mpReactionVar);
+        // }
 
         rSerializer.load("mis_defined_DensityVar",mis_defined_DensityVar);
         rSerializer.load("mis_defined_DiffusionVar",mis_defined_DiffusionVar);
@@ -641,6 +652,43 @@ private:
         rSerializer.load("mis_defined_VelocityVar",mis_defined_VelocityVar);
         rSerializer.load("mis_defined_SpecificHeatVar",mis_defined_SpecificHeatVar);
         rSerializer.load("mis_defined_ReactionVar",mis_defined_ReactionVar);
+
+        if(mis_defined_DensityVar) {
+            rSerializer.load("DensityVar",mpDensityVar);
+        }
+        if(mis_defined_DiffusionVar) {
+            rSerializer.load("DiffusionVar",mpDiffusionVar);
+        }
+        if(mis_defined_UnknownVar) {
+            rSerializer.load("UnknownVar",mpUnknownVar);
+        }
+        if(mis_defined_VolumeSourceVar) {
+            rSerializer.load("VolumeSourceVar",mpVolumeSourceVar);
+        }
+        if(mis_defined_SurfaceSourceVar) {
+            rSerializer.load("SurfaceSourceVar",mpSurfaceSourceVar);
+        }
+        if(mis_defined_ProjectionVar) {
+            rSerializer.load("ProjectionVar",mpProjectionVar);
+        }
+        if(mis_defined_ConvectionVar) {
+            rSerializer.load("ConvectionVar",mpConvectionVar);
+        }
+        if(mis_defined_MeshVelocityVar) {
+            rSerializer.load("MeshVelocityVar",mpMeshVelocityVar);
+        }
+        if(mis_defined_TransferCoefficientVar) {
+            rSerializer.load("TransferCoefficientVar",mpTransferCoefficientVar);
+        }
+        if(mis_defined_VelocityVar) {
+            rSerializer.load("VelocityVar",mpVelocityVar);
+        }
+        if(mis_defined_SpecificHeatVar) {
+            rSerializer.load("SpecificHeatVar",mpSpecificHeatVar);
+        }
+        if(mis_defined_ReactionVar) {
+            rSerializer.load("ReactionVar",mpReactionVar);
+        }
 
     }
 
